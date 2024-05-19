127b2b9c7faa85c4c43fc16cd811574e82a8e059
kik <kik@DESKTOP-I92ODLI>
2024-05-19 23:49:12 +0800

    Add project files.

---
+++ SportsFit/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsFit.Models;
+
+namespace SportsFit.DAL
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+		public virtual DbSet<Product> Products { get; set; }
+	}
+}
